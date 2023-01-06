c4e3af021e7d0c6fd7e8602dd129816eb37cce11
Fernando Chicaiza <washopilot@yahoo.com>
2023-01-05 22:41:09 -0500

    primer commit

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        /* don't expose password or any sensitive info, done only for demo */
+        host: 'localhost',
+        user: 'root',
+        password: 'root1979',
+        database: 'mysql_db'
+    },
+    listPerPage: 10
+};
+module.exports = config;
